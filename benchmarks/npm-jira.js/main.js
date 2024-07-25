05f27e667763aa4ba275b302396481bf6ed676b0
urielch <uriel.chemouni@gmail.com>
2024-07-25 11:12:54 +0200

    add bench 2

--- benchmarks/npm-jira.js/main.js
+++ benchmarks/npm-jira.js/main.js
@@ -1,7 +1,9 @@
-import { Version3Client } from 'npm:jira.js';
-import { readFileSync } from 'node:fs';
+import { Version3Client } from "jira.js";
+import { readFileSync } from "fs";
 
-const certs = Object.fromEntries(readFileSync("../../.env", "utf8").split("\n").map((line) => line.split("=")));
+const certs = Object.fromEntries(
+  readFileSync("../../.env", "utf8").split("\n").map((line) => line.split("=")),
+);
 
 const host = certs.JRA_BASE;
 const email = certs.JRA_USER;
@@ -17,41 +19,7 @@ const client = new Version3Client({
   },
 });
 
-async function main() {
-  const projects = await client.projects.searchProjects();
-
-  if (projects.values.length) {
-    const project = projects.values[0];
-
-    const { id } = await client.issues.createIssue({
-      fields: {
-        summary: 'My first issue',
-        issuetype: {
-          name: 'Task'
-        },
-        project: {
-          key: project.key,
-        },
-      }
-    });
-
-    const issue = await client.issues.getIssue({ issueIdOrKey: id });
-
-    console.log(`Issue '${issue.fields.summary}' was successfully added to '${project.name}' project.`);
-  } else {
-    const myself = await client.myself.getCurrentUser();
-
-    const { id } = await client.projects.createProject({
-      key: 'PROJECT',
-      name: "My Project",
-      leadAccountId: myself.accountId,
-      projectTypeKey: 'software',
-    });
-
-    const project = await client.projects.getProject({ projectIdOrKey: id.toString() });
-
-    console.log(`Project '${project.name}' was successfully created.`);
-  }
+const dashboards = await client.dashboards.getAllDashboards()
+for (const dashboard of dashboards.dashboards) {
+  console.log(dashboard);
 }
-
-main();
